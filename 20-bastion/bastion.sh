bc13620af2608fcff0c91ed023fe72f8d05e8b74
NArunkumar <nelluri.arunkumar@gmail.com>
2025-09-13 17:58:34 +0530

    Creating Roboshop Infra using Terraform

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -4,9 +4,18 @@ yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum -y install terraform
 
-growpart /dev/nvme0n1 4
-lvextend -L +20G /dev/RootVG/rootvol
-lvextend -L +20G /dev/RootVG/homevol
+##Below commands are used for t3.micro
+# growpart /dev/nvme0n1 4
+# lvextend -L +20G /dev/RootVG/rootvol
+# lvextend -L +20G /dev/RootVG/homevol
 
+# xfs_growfs /
+# xfs_growfs /home
+
+###For instance type t2.micro below are the commands to extend the file sizes
+sudo growpart /dev/xvda 4
+sudo pvresize /dev/xvda4
+lvextend -L 20G /dev/RootVG/rootVol
+lvextend -L 20G /dev/RootVG/homeVol
 xfs_growfs /
 xfs_growfs /home
